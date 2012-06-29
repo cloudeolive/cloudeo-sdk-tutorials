29c8d388d7c58764d7d7b8d4acadf1fd3cc36001
Tadeusz Kozak <stoiczek@gmail.com>
2012-06-29 18:09:23 +0200

    Updated the first tutorial, to use new initialization API

--- JavaScript/Tutorial1/scripts.js
+++ JavaScript/Tutorial1/scripts.js
@@ -13,20 +13,39 @@
 CDOT.onDomReady = function () {
   log.debug('DOM loaded');
   CDOT.initCloudeoLogging();
-  CDOT.initializeCloudeoServiceContainer();
+  CDOT.initializeCloudeo();
 };
 
 /**
  * Initializes the CloudeoServiceContainer.
  */
-CDOT.initializeCloudeoServiceContainer = function () {
-  log.debug("Initializing the Cloudeo Service Container");
-  CDOT.cloudeoServiceContainer = new CDO.CloudeoPlugin(CDOT.PLUGIN_CONTAINER_ID);
-  if (CDOT.cloudeoServiceContainer.loadPlugin()) {
-    CDOT.createCloudeoService();
-  } else {
-    CDOT.showInstallButton();
-  }
+CDOT.initializeCloudeo = function () {
+  log.debug("Initializing the Cloudeo SDK");
+  var initListener = new CDO.PlatformInitListener();
+  initListener.onInitProgressChanged = function (e) {
+    log.debug("Platform init progress: " + e.progress);
+  };
+
+  initListener.onInitStateChanged = function (e) {
+    switch (e.state) {
+      case CDO.InitState.ERROR:
+        log.error("Failed to initialize the Cloudeo SDK");
+        log.error("Reason: " + e.errMessage + ' (' + e.errCode + ')');
+        break;
+      case CDO.InitState.INITIALIZED:
+        CDOT.getVersion();
+        break;
+      case CDO.InitState.INSTALLATION_REQUIRED:
+        CDOT.showInstallButton(e.installerURL);
+        break;
+      case CDO.InitState.BROWSER_RESTART_REQUIRED:
+        log.debug("Please restart your browser in order to complete platform auto-update");
+        break;
+      default:
+        log.error("Got unsupported init state: " + e.state);
+    }
+  };
+  CDO.initPlatform(initListener);
 };
 
 
@@ -42,73 +61,25 @@ CDOT.initializeCloudeoServiceContainer = function () {
 /**
  * Fetches the installer URL and shows the Cloudeo Plugin installation URL.
  */
-CDOT.showInstallButton = function () {
+CDOT.showInstallButton = function (url) {
   log.debug("Cloudeo plugin not installed; Getting the installation URL.");
-  var responder = CDO.createResponder(CDOT.onInstallerUrl);
-  CDO.getInstallerURL(responder)
-};
-
-CDOT.onInstallerUrl = function (url) {
-  log.debug("Cloudeo Plug-in Installed fetched. Showing the install button.");
   $('#installBtn').
       attr('href', url).
-      css('display','block').
-      click(CDOT.onInstallBtnClicked);
-};
-
-CDOT.onInstallBtnClicked = function () {
-  log.debug("Install button clicked. Starting polling for the plug-in availability");
-  CDOT.cloudeoServiceContainer.startPolling(function () {
-    log.debug("Plugin successfully installed. Initializing.");
-    CDOT.createCloudeoService();
-    $('#installBtn').css('display','none');
-  });
-};
-
-/**
- * ==========================================================================
- * Cloudeo Service creation
- * ==========================================================================
- */
-
-/**
- * Creates the Cloudeo Service. Should be called upon successful initialization
- * of the Cloudeo Service Container.
- */
-CDOT.createCloudeoService = function () {
-  log.debug("Creating Cloudeo Service.");
-  var responder = CDO.createResponder(
-      CDOT.onCloudeoServiceCreated,
-      CDOT.onCloudeoServiceCreateError);
-  CDOT.cloudeoServiceContainer.createService(responder)
+      css('display','block');
 };
 
 /**
  *
  * @param {CDO.CloudeoService} service
  */
-CDOT.onCloudeoServiceCreated = function (service) {
-  log.debug("Cloudeo service created");
-  CDOT.service = service;
-  var responder = CDO.createResponder(CDOT.onGetVersionResult);
-  CDOT.service.getVersion(responder);
-};
+CDOT.getVersion = function () {
+  var getVersionResult = function(version) {
+    log.debug("Cloudeo service version: " + version);
+    $('#sdkVersion').html(version);
+  };
 
-/**
- *
- * @param {CDO.CloudeoService} service
- */
-CDOT.onCloudeoServiceCreateError = function (errCode, errMessage) {
-  log.error("Failed to create Cloudeo Service, due to: ");
-  log.error(errMessage + ' (' + errCode + ')');
-};
-
-/**
- *
- * @param {string} version
- */
-CDOT.onGetVersionResult = function (version) {
-  log.debug("Cloudeo service version: " + version);
+  var responder = CDO.createResponder(getVersionResult);
+  CDO.getService().getVersion(responder);
 };
 
 
