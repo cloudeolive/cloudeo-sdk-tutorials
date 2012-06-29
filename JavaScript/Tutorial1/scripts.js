ebcf5dc117d7839d2762bb39d455e323fe806266
Tadeusz Kozak <stoiczek@gmail.com>
2012-06-29 18:32:53 +0200

    Simplified the scripts

--- JavaScript/Tutorial1/scripts.js
+++ JavaScript/Tutorial1/scripts.js
@@ -25,65 +25,66 @@ CDOT.onDomReady = function () {
  */
 CDOT.initializeCloudeo = function () {
   log.debug("Initializing the Cloudeo SDK");
+
+//  Step 1 - create the PlatformInitListener and overwrite it's methods.
   var initListener = new CDO.PlatformInitListener();
+
+//  Define the handler for initialization progress changes
   initListener.onInitProgressChanged = function (e) {
     log.debug("Platform init progress: " + e.progress);
     $("#initProgressBar").progressbar('value', e.progress);
   };
 
+//  Define the handler for initialization state changes
   initListener.onInitStateChanged = function (e) {
     switch (e.state) {
+
       case CDO.InitState.ERROR:
+//      After receiving this status, the initialization is stopped as due to
+//      a failure.
         log.error("Failed to initialize the Cloudeo SDK");
         log.error("Reason: " + e.errMessage + ' (' + e.errCode + ')');
         break;
+
       case CDO.InitState.INITIALIZED:
-        CDOT.getVersion();
+//      This state flag indicates that the Cloudeo SDK is initialized and fully
+//      functional. In this tutorial, we will just perform sample call to
+//      retrieve the current version of the SDK
+        var getVersionResult = function (version) {
+          log.debug("Cloudeo service version: " + version);
+          $('#sdkVersion').html(version);
+        };
+
+        var responder = CDO.createResponder(getVersionResult);
+        CDO.getService().getVersion(responder);
         break;
+
       case CDO.InitState.INSTALLATION_REQUIRED:
-        CDOT.showInstallButton(e.installerURL);
+//      Current user doesn't have the Cloudeo Plug-In installed and it is
+//      required - use provided URL to ask the user to install the Plug-in.
+//      Note that the initialization process is just frozen in this state -
+//      the SDK polls for plug-in availability and when it becomes available,
+//      continues with the initialization.
+        $('#installBtn').
+            attr('href', e.installerURL).
+            css('display', 'block');
         break;
+
       case CDO.InitState.BROWSER_RESTART_REQUIRED:
+//      This state indicates that Cloudeo SDK performed auto-update and in order
+//      to accomplish this process, browser needs to be restarted.
         log.debug("Please restart your browser in order to complete platform auto-update");
         break;
+
       default:
+//      Default handler, just for sanity
         log.error("Got unsupported init state: " + e.state);
     }
   };
+
+//  Step 2. Actually trigger the asynchronous initialization of the Cloudeo SDK.
   CDO.initPlatform(initListener);
 };
-
-
-/**
- * ==========================================================================
- * Plug-in installation
- * ==========================================================================
- */
-
-/**
- * Fetches the installer URL and shows the Cloudeo Plugin installation URL.
- */
-CDOT.showInstallButton = function (url) {
-  log.debug("Cloudeo plugin not installed; Getting the installation URL.");
-  $('#installBtn').
-      attr('href', url).
-      css('display', 'block');
-};
-
-/**
- *
- * @param {CDO.CloudeoService} service
- */
-CDOT.getVersion = function () {
-  var getVersionResult = function (version) {
-    log.debug("Cloudeo service version: " + version);
-    $('#sdkVersion').html(version);
-  };
-
-  var responder = CDO.createResponder(getVersionResult);
-  CDO.getService().getVersion(responder);
-};
-
 
 /**
  * Register the document ready handler.
