39862e78fdb0176bcd3725793678f73a7d04793b
Tadeusz Kozak <stoiczek@gmail.com>
2012-07-03 15:40:50 +0200

    Added microphone gain control, monitoring of mic activity and handler for devices list changed, with new devices propagation

--- JavaScript/Tutorial3/scripts.js
+++ JavaScript/Tutorial3/scripts.js
@@ -27,16 +27,52 @@ CDOT.initUI = function () {
                                   value:127,
                                   slide:CDOT.onVolumeSlide
                                 });
+  $('#micGainCtrlSlider').slider({
+                                   min:0,
+                                   max:255,
+                                   animate:true,
+                                   value:127,
+                                   slide:CDOT.onMicGainSlide
+                                 });
+  $('#micActivityBar').progressbar({value:50});
   $('#playTestSoundBtn').click(CDOT.onPlayTestSoundBtnClicked);
+  $('#micActivityEnabledChckbx').change(CDOT.onMicActivityEnabledChckbxChange);
 };
 
 
 CDOT.onPlatformReady = function () {
+  CDOT.initializeListener();
   CDOT.populateDevicesQuick();
   CDOT.populateVolume();
+  CDOT.populateMicGain();
   $('#playTestSoundBtn').
       click(CDOT.onPlayTestSoundBtnClicked).
       removeClass('disabled');
+};
+
+CDOT.initializeListener = function () {
+  var listener = new CDO.CloudeoServiceListener();
+  listener.onDeviceListChanged = function (e) {
+    log.debug("Got devices list changed");
+    if(e.audioInChanged) {
+      log.debug("Got new microphone plugged in");
+      CDOT.populateDevicesOfType('#micSelect', 'AudioCapture');
+    }
+    if(e.audioOutChanged) {
+      log.debug("Got new speakers plugged in");
+      CDOT.populateDevicesOfType('#spkSelect', 'AudioOutput');
+    }
+    if(e.videoInChanged) {
+      log.debug("Got new camera plugged in");
+      CDOT.populateDevicesOfType('#camSelect', 'VideoCapture');
+    }
+  };
+
+  listener.onMicActivity = function (e) {
+    log.debug("Got mic activity: " + e.activity);
+    $('#micActivityBar').progressbar('value', e.activity / 255 * 100);
+  };
+  CDO.getService().addServiceListener(CDO.createResponder(), listener);
 };
 
 CDOT.populateVolume = function () {
@@ -46,12 +82,28 @@ CDOT.populateVolume = function () {
   CDO.getService().getSpeakersVolume(CDO.createResponder(resultHandler));
 };
 
+CDOT.populateMicGain = function () {
+  var resultHandler = function (volume) {
+    $('#micGainCtrlSlider').slider('value', volume);
+  };
+  CDO.getService().getMicrophoneVolume(CDO.createResponder(resultHandler));
+};
+
 CDOT.onVolumeSlide = function (e, ui) {
   CDO.getService().setSpeakersVolume(CDO.createResponder(), ui.value);
 };
 
+CDOT.onMicGainSlide = function (e, ui) {
+  CDO.getService().setMicrophoneVolume(CDO.createResponder(), ui.value);
+};
+
 CDOT.onPlayTestSoundBtnClicked = function () {
   CDO.getService().startPlayingTestSound(CDO.createResponder());
+};
+
+CDOT.onMicActivityEnabledChckbxChange = function () {
+  var enabled = $(this).is(':checked');
+  CDO.getService().monitorMicActivity(CDO.createResponder(), enabled);
 };
 
 /**
