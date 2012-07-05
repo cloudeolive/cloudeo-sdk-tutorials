3e2220ee9675d625ecbcd8a5f04bfc975cd4f6fe
Tadeusz Kozak <stoiczek@gmail.com>
2012-07-05 11:06:01 +0200

    Tutorial 4 clean up

--- JavaScript/Tutorial4/scripts.js
+++ JavaScript/Tutorial4/scripts.js
@@ -12,17 +12,10 @@
  */
 CDOT.onDomReady = function () {
   log.debug('DOM loaded');
-  CDOT.initUI();
   CDOT.initCloudeoLogging();
   CDOT.initDevicesSelects();
   CDOT.initializeCloudeoQuick(CDOT.onPlatformReady);
 };
-
-
-CDOT.initUI = function () {
-
-};
-
 
 CDOT.onPlatformReady = function () {
   CDOT.populateDevicesOfType('#camSelect', 'VideoCapture');
@@ -57,12 +50,6 @@ CDOT.startLocalVideo = function () {
                    });
   };
   CDO.getService().startLocalVideo(CDO.createResponder(resultHandler));
-};
-
-CDOT.initializeListener = function () {
-  var listener = new CDO.CloudeoServiceListener();
-
-  CDO.getService().addServiceListener(CDO.createResponder(), listener);
 };
 
 /**
